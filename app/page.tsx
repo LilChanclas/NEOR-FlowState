f07980c33ba7066015a337414a96e7d64694501b
LilChanclas <rosariohumbertozz@gmail.com>
2026-09-10 09:26:34 -0700

    sample de dropdown

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import Dropdown from "./components/Dropdown";
+
 export default function Home() {
   return (
     <section className="bg-gray-800 w-full h-full flex">
-      <p className="m-auto text-white">AQUI VA UN TABLERO TIPO KANBAN</p>
+      <div className="">
+        <Dropdown></Dropdown>
+         <p className="m-auto text-white">AQUI VA UN TABLERO TIPO KANBAN</p>
+      </div>
     </section>
   );
 }
